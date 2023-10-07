657422a76391b8887169b564f657d6f3684377f2
Umesh <yomyouronlinemart@gmail.com>
2023-10-07 12:58:43 +0530

    fw23_0249_b1

---
+++ Backend/candidbot/candidbot/src/main/java/com/candidbot/constants/Constants.java
@@ -0,0 +1,5 @@
+package com.candidbot.constants;
+
+public class Constants {
+
+}
---
+++ Backend/candidbot/candidbot/src/test/java/com/candidbot/CandidbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candidbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
